d92515a9868c567fd6d8f0a3b1b754253884cada
Alano Teles <alanoteles@gmail.com>
2024-10-29 20:57:15 -0300

    Finished unit tests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,12 +14,12 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -29,11 +29,6 @@ export class UsersController {
         private usersService: UsersService,
         private authService: AuthService) {}
 
- 
-    // @Get('whoAmI')
-    // whoAmI(@Session() session: any) {
-    //     return this.usersService.findOne(session.userId)
-    // }
 
     @Get('whoAmI')
     @UseGuards(AuthGuard)
