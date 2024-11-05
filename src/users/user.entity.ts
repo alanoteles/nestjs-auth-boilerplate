080ff3fa6ad922a76584b5e2c80f67a4b65491be
Alano Teles <alanoteles@gmail.com>
2024-11-05 08:05:48 -0300

    Added authorization guards

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from 'typeorm'
+import { Report } from 'src/reports/report.entity'
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove, OneToMany } from 'typeorm'
 
 @Entity()
 export class User {
@@ -11,6 +12,12 @@ export class User {
 
     @Column()
     password: string
+
+    @Column({ default: true })
+    admin: boolean
+
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[]
 
     @AfterInsert()
     logInsert() {
